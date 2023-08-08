f7d0f1fb7168b32f519a5a1278d85f516345de64
Brian.B <brtl765@gmail.com>
2023-08-08 17:52:45 -0400

    added nav menu sandwich

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Logo from './components/Logo/Logo';
-import { DropDown } from './components/Dropdown/Dropdown';
+import DropDown from './components/Dropdown/Dropdown';
 import './App.css';
 
 
--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Dropdown from 'react-bootstrap/Dropdown';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faBars);
 
 function DropDown() {
   return (
     <Dropdown>
       <Dropdown.Toggle variant="success" id="dropdown-basic">
-        Dropdown Button
+      <FontAwesomeIcon icon="{faBars}" />
       </Dropdown.Toggle>
 
       <Dropdown.Menu>
